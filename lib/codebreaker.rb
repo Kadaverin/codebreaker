b275a36fa11f39a6d95fa64eb94be07cb8bc8f0c
Danil Davydov <Linadakill@gmail.com>
2018-06-22 20:28:17 +0300

    delete unnecessary code

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -2,5 +2,3 @@ require_relative 'codebreaker/version'
 require_relative 'codebreaker/game'
 require_relative 'codebreaker/game_interface'
 require_relative 'codebreaker/constants'
-
-Codebreaker::GameInterface.new.start_game
